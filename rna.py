d845e4e0bae5ce6f80b436b8fc017d3cdf638a69
Alexander <144268680+GlalexHSE@users.noreply.github.com>
2023-11-08 19:20:18 +0300

    rna.py
    Dictionary is TOP!

--- rna.py
+++ rna.py
@@ -1,1 +1,10 @@
-
+def to_rna(dna_strand):
+    """Вычисляет результат транскрипции РНК
+    :param dna_strand: строка ДНК из символов ACGT
+    :return: строка РНК из символов UGCA
+    """
+    nucleotide_map = {"A": "U", "C": "G", "G": "C", "T": "A"}
+    rna_strand = ""
+    for i in dna_strand:
+        rna_strand += nucleotide_map.get(i, "")
+    return rna_strand
